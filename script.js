1cd1650a1c6484e0001836177cd9db719694ee8f
Alexandre <alex67470@gmail.com>
2026-01-26 11:39:04 +0100

    add(feat) add archievements

--- script.js
+++ script.js
@@ -143,19 +143,77 @@ function showToast(text) {
   }, 2000);
 }
 
-let achCommis1 = false;
 let achSoup100 = false;
+let achSoup300 = false;
+let achSoup500 = false;
+let achSoup1000 = false;
+let achSoup2000 = false;
 let achClick50 = false;
+let achClick100 = false;
+let achClick200 = false;
+let achClick300 = false;
+let achClick400 = false;
+let achClick500 = false;
 let achSpoon1 = false;
+let achSpoon2 = false;
+let achSpoon3 = false;
+let achSpoon4 = false;
+let achSpoon5 = false;
+let achCommis1 = false;
+let achCommis2 = false;
+let archCommis3 = false;
+let archCommis4 = false;
+let archCommis5 = false;
+let achPot1 = false;
+let achPot2 = false;
+let achPot3 = false;
+let achPot4 = false;
+let achPot5 = false;
 
 function checkAchievements() {
   if (!achSoup100 && totalSoups >= 100) {
     achSoup100 = true;
     showToast("Succès débloqué : 100 d'une longue lignée !");
   }
+  if (!achSoup300 && totalSoups >= 300) {
+    achSoup300 = true;
+    showToast("Succès débloqué : 300 soupes, ça en fait une grande famille !");
+  }
+  if (!achSoup500 && totalSoups >= 500) {
+    achSoup500 = true;
+    showToast("Succès débloqué : 500 soupes, bientôt un banquet !");
+  }
+  if (!achSoup1000 && totalSoups >= 1000) {
+    achSoup1000 = true;
+    showToast("Succès débloqué : 1000 soupes, vous avez une armée ?!");
+  }
+  if (!achSoup2000 && totalSoups >= 2000) {
+    achSoup2000 = true;
+    showToast("Succès débloqué : 2000 soupes, ça n'en fait pas un nimbus !");
+  }
   if (!achClick50 && totalClicks >= 50) {
     achClick50 = true;
     showToast("Succès débloqué : 50 touilles !");
+  }
+  if (!achClick100 && totalClicks >= 100) {
+    achClick100 = true;
+    showToast("Succès débloqué : 100 touilles !");
+  }
+  if (!achClick200 && totalClicks >= 200) {
+    achClick200 = true;
+    showToast("Succès débloqué : 200 touilles (ça va le doigt ?) !");
+  }
+  if (!achClick300 && totalClicks >= 300) {
+    achClick300 = true;
+    showToast("Succès débloqué : 300 touilles (J'ai des doigts en acier trempé !)");
+  }
+  if (!achClick400 && totalClicks >= 400) {
+    achClick400 = true;
+    showToast("Succès débloqué : 400 touilles (T'avais la ref du précédent ?) !");
+  }
+  if (!achClick500 && totalClicks >= 500) {
+    achClick500 = true;
+    showToast("Succès débloqué : 500 touilles (Tu vas finir sourd) !");
   }
   if (!achSpoon1 && spoonLevel >= 1) {
     achSpoon1 = true;
@@ -164,5 +222,57 @@ function checkAchievements() {
   if (!achCommis1 && commisLevel >= 1) {
   achCommis1 = true;
   showToast("Succès débloqué : Premier es-*tousse* commis !");
-}
+ }
+ if (!achSpoon2 && spoonLevel >= 10) {
+  achSpoon2 = true;
+  showToast("Succès débloqué : En réalité, ça n'en ajoute pas vraiment plus !");
+ }
+  if (!achSpoon3 && spoonLevel >= 25) {
+    achSpoon3 = true;
+    showToast("Succès débloqué : Cuillères à profusion !");
+  }
+  if (!achSpoon4 && spoonLevel >= 50) {
+    achSpoon4 = true;
+    showToast("Succès débloqué : Maître cuillier, une leçon de Zen !");
+  }
+  if (!achSpoon5 && spoonLevel >= 100) {
+    achSpoon5 = true;
+    showToast("Succès débloqué : L'ultime cuillère !");
+  }
+  if (!achCommis2 && commisLevel >= 5) {
+    achCommis2 = true;
+    showToast("Succès débloqué : Mes commis sont mes amis !");
+  }
+  if (!archCommis3 && commisLevel >= 10) {
+    archCommis3 = true;
+    showToast("Succès débloqué : On part en conquête !");
+  }
+  if (!archCommis4 && commisLevel >= 20) {
+    archCommis4 = true;
+    showToast("Succès débloqué : Bientôt la révolution des soupiers !");
+  }
+  if (!archCommis5 && commisLevel >= 50) {
+    archCommis5 = true;
+    showToast("Succès débloqué : Bienvenue en enfer !");
+  }
+  if (!achPot1 && potLevel >= 1) {
+    achPot1 = true;
+    showToast("Succès débloqué : Premier chaudron !");
+  }
+  if (!achPot2 && potLevel >= 5) {
+    achPot2 = true;
+    showToast("Succès débloqué : La soupe bout !");
+  }
+  if (!achPot3 && potLevel >= 10) {
+    achPot3 = true;
+    showToast("Succès débloqué : Marmite géante !");
+  }
+  if (!achPot4 && potLevel >= 25) {
+    achPot4 = true;
+    showToast("Succès débloqué : Océan de soupe !");
+  }
+  if (!achPot5 && potLevel >= 50) {
+    achPot5 = true;
+    showToast("Succès débloqué : Big Bang de soupe !");
+  }
 }
